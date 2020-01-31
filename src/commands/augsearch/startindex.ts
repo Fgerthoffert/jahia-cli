a479e040c238327ce3b0a79b1ab30bd30b5ac1b2
Francois Gerthoffert <f.gerthoffert@gmail.com>
2020-01-31 08:33:43 -0500

    Added command to trigger indexing

---
+++ src/commands/augsearch/startindex.ts
@@ -0,0 +1,57 @@
+import { flags } from '@oclif/command';
+import { performance } from 'perf_hooks';
+
+import Command from '../../base';
+
+import launchPuppeteer from '../../utils/puppeteer/launch';
+import closePuppeteer from '../../utils/puppeteer/close';
+import graphqlClient from '../../utils/graphql/client';
+import waitAlive from '../../utils/waitAlive';
+import openJahia from '../../utils/openJahia';
+import navPage from '../../utils/navPage';
+
+import { sleep } from '../../utils'
+
+export default class AugsearchStartindex extends Command {
+  static description = 'Trigger the indexing job once the site has been imported';
+
+  static flags = {
+    ...Command.flags,
+    help: flags.help({ char: 'h' }),
+  };
+
+  static args = [{ name: 'file' }];
+
+  async run() {
+    const { flags } = this.parse(AugsearchStartindex);
+    const t0 = performance.now();
+
+    const gClient = await graphqlClient(flags);
+    await waitAlive(gClient, 500000); // Wait for 500s by default
+    const browser = await launchPuppeteer(!flags.debug);
+    const jahiaPage = await openJahia(browser, flags);
+
+    await navPage(
+      jahiaPage,
+      flags.jahiaAdminUrl +
+        '/cms/adminframe/default/en/settings.augmentedsearch.html',
+    );
+
+    await jahiaPage.select('#esConnectionId', 'augmented-search-conn');
+    const saveButton = await jahiaPage.$('button[name="_eventId_save"]');
+    await Promise.all([jahiaPage.waitForNavigation(), saveButton.click()]);
+
+    await sleep(2000);
+
+    const indexButton = await jahiaPage.$('button[name="_eventId_reindex"]');
+    await Promise.all([jahiaPage.waitForNavigation(), indexButton.click()]);
+
+    await jahiaPage.close();
+    await closePuppeteer(browser);
+
+    const t1 = performance.now();
+    console.log(
+      'Total Exceution time: ' + Math.round(t1 - t0) + ' milliseconds.',
+    );
+  }
+}
