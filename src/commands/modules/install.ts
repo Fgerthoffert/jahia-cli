c1d9ef06efc9b5738fa056875a844f48115143c0
Francois Gerthoffert <f.gerthoffert@gmail.com>
2020-02-18 15:27:23 -0500

    Added flag to force module installation

--- src/commands/modules/install.ts
+++ src/commands/modules/install.ts
@@ -12,49 +12,49 @@ import { exit } from '@oclif/errors';
 import installModule from '../../components/modules/install';
 
 export default class ModulesInstall extends Command {
-  static description = 'Installs a module';
+	static description = 'Installs a module';
 
-  static flags = {
-    ...Command.flags,
-    help: flags.help({ char: 'h' }),
-    file: flags.string({
-      required: true,
-      description:
-        'Specify the filepath to the module to be installed (jar on filesystem)',
-    }),
-    id: flags.string({
-      required: true,
-      description: 'Module Id (string or comma separated strings)',
-    }),
-    version: flags.string({
-      description: 'Specify the module version to be installed',
-    }),
-  };
+	static flags = {
+		...Command.flags,
+		help: flags.help({ char: 'h' }),
+		file: flags.string({
+			required: true,
+			description: 'Specify the filepath to the module to be installed (jar on filesystem)'
+		}),
+		id: flags.string({
+			required: true,
+			description: 'Module Id (string or comma separated strings)'
+		}),
+		force: flags.boolean({
+			description: 'Forces modules installation (even if already installed)'
+		}),
+		version: flags.string({
+			description: 'Specify the module version to be installed'
+		})
+	};
 
-  static args = [{ name: 'file' }];
+	static args = [ { name: 'file' } ];
 
-  async run() {
-    const { flags } = this.parse(ModulesInstall);
-    const t0 = performance.now();
+	async run() {
+		const { flags } = this.parse(ModulesInstall);
+		const t0 = performance.now();
 
-    if (flags.file === undefined) {
-      console.log('ERROR: Please specify a filepath');
-      exit();
-    }
+		if (flags.file === undefined) {
+			console.log('ERROR: Please specify a filepath');
+			exit();
+		}
 
-    if (!fs.existsSync(flags.file)) {
-      console.log('ERROR: Unable to access file: ' + flags.file);
-      exit();
-    }
+		if (!fs.existsSync(flags.file)) {
+			console.log('ERROR: Unable to access file: ' + flags.file);
+			exit();
+		}
 
-    const gClient = await graphqlClient(flags);
-    await waitAlive(gClient, 500000); // Wait for 500s by default
+		const gClient = await graphqlClient(flags);
+		await waitAlive(gClient, 500000); // Wait for 500s by default
 
-    await installModule(flags, flags.file, flags.id);
+		await installModule(flags, flags.file, flags.id, flags.force);
 
-    const t1 = performance.now();
-    console.log(
-      'Total Exceution time: ' + Math.round(t1 - t0) + ' milliseconds.',
-    );
-  }
+		const t1 = performance.now();
+		console.log('Total Exceution time: ' + Math.round(t1 - t0) + ' milliseconds.');
+	}
 }
