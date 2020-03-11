b1cb25e6b0d7615156237ab7afc29de47bffbabd
Francois Gerthoffert <f.gerthoffert@gmail.com>
2020-03-11 14:40:26 -0400

    Added handling for authorization

--- src/commands/assets/load.ts
+++ src/commands/assets/load.ts
@@ -15,60 +15,67 @@ import openJahia from '../../utils/openJahia';
 import { exit } from '@oclif/errors';
 
 export default class LoadAssets extends Command {
-  static description =
-    'Download assets from a manifest to the local filesystem';
+	static description = 'Download assets from a manifest to the local filesystem';
 
-  static flags = {
-    ...Command.flags,
-    help: flags.help({ char: 'h' }),
-    manifest: flags.string({
-      required: true,
-      description: 'Specify the filepath to the manifest',
-    }),
-  };
+	static flags = {
+		...Command.flags,
+		help: flags.help({ char: 'h' }),
+		manifest: flags.string({
+			required: true,
+			description: 'Specify the filepath to the manifest'
+		}),
+		username: flags.string({
+			description: 'Username for basic auth'
+		}),
+		password: flags.string({
+			description: 'Password for basic auth'
+		})
+	};
 
-  static args = [{ name: 'file' }];
+	static args = [ { name: 'file' } ];
 
-  async run() {
-    const { flags } = this.parse(LoadAssets);
-    const t0 = performance.now();
+	async run() {
+		const { flags } = this.parse(LoadAssets);
+		const t0 = performance.now();
 
-    if (flags.manifest === undefined) {
-      console.log('ERROR: Please specify a filepath');
-      exit();
-    }
-    const manifestContent = await loadYamlFile(flags.manifest);
-    if (
-      manifestContent.assets !== undefined &&
-      manifestContent.assets.length > 0
-    ) {
-      const gClient = await graphqlClient(flags);
-      await waitAlive(gClient, 500000); // Wait for 500s by default
-      const browser = await launchPuppeteer(!flags.debug);
-      const jahiaPage = await openJahia(browser, flags);
+		if (flags.manifest === undefined) {
+			console.log('ERROR: Please specify a filepath');
+			exit();
+		}
+		const manifestContent = await loadYamlFile(flags.manifest);
+		if (manifestContent.assets !== undefined && manifestContent.assets.length > 0) {
+			const gClient = await graphqlClient(flags);
+			await waitAlive(gClient, 500000); // Wait for 500s by default
+			const browser = await launchPuppeteer(!flags.debug);
+			const jahiaPage = await openJahia(browser, flags);
 
-      for (const jahiaAsset of manifestContent.assets) {
-        if (jahiaAsset.type === 'http') {
-          console.log(
-            'Loading: ' + jahiaAsset.source + ' into: ' + jahiaAsset.filepath,
-          );
-          // eslint-disable-next-line no-await-in-loop
-          await download(jahiaAsset.source).then(data => {
-            fs.writeFileSync(jahiaAsset.filepath, data);
-          });
-        } else {
-          console.log('WARNING: Asset type not supported');
-        }
-      }
+			for (const jahiaAsset of manifestContent.assets) {
+				if (jahiaAsset.type === 'http') {
+					console.log('Loading: ' + jahiaAsset.source + ' into: ' + jahiaAsset.filepath);
+					let options = {};
+					if (flags.username !== undefined && flags.password !== undefined) {
+						options = {
+							headers: {
+								Authorization:
+									'Basic ' + Buffer.from(flags.username + ':' + flags.password).toString('base64')
+							}
+						};
+					}
+					// eslint-disable-next-line no-await-in-loop
+					await download(jahiaAsset.source, undefined, options).then((data) => {
+						fs.writeFileSync(jahiaAsset.filepath, data);
+					});
+				} else {
+					console.log('WARNING: Asset type not supported');
+				}
+			}
 
-      await jahiaPage.close();
-      await closePuppeteer(browser);
-    } else {
-      console.log('Manifest is empty and does not contain any modules');
-    }
-    const t1 = performance.now();
-    console.log(
-      'Total Exceution time: ' + Math.round(t1 - t0) + ' milliseconds.',
-    );
-  }
+			await jahiaPage.close();
+			await closePuppeteer(browser);
+		} else {
+			console.log('Manifest is empty and does not contain any modules');
+		}
+		const t1 = performance.now();
+		console.log('Total Exceution time: ' + Math.round(t1 - t0) + ' milliseconds.');
+	}
 }
