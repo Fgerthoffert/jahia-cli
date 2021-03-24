65aa4fee2537428d05d6a591ccdc96287e38f364
Francois G <f.gerthoffert@gmail.com>
2021-03-24 10:45:16 -0400

    Update index.ts

--- src/components/modules/install/index.ts
+++ src/components/modules/install/index.ts
@@ -18,18 +18,18 @@ const installModule = async (
 	// ModuleId is undefined, we blindly push the module but don't check for proper installation
 	if (moduleId === undefined) {
 		console.log('Submitting: ' + moduleFilepath + ' for installation');
-		await installMod(flags.jahiaAdminUrl, flags.jahiaAdminUsername, flags.jahiaAdminUsername, moduleFilepath);
+		await installMod(flags.jahiaAdminUrl, flags.jahiaAdminUsername, flags.jahiaAdminPassword, moduleFilepath);
 	} else {
 		const installedModules = await getModules(
 			flags.jahiaAdminUrl,
 			flags.jahiaAdminUsername,
-			flags.jahiaAdminUsername
+			flags.jahiaAdminPassword
 		);
 		
 		if (isInstalled(installedModules, moduleId, moduleVersion) === false || force === true) {
 			// Check if there is a different version of that module installed
 			console.log('Module needs to be installed');
-			const installedModule = await installMod(flags.jahiaAdminUrl, flags.jahiaAdminUsername, flags.jahiaAdminUsername, moduleFilepath);
+			const installedModule = await installMod(flags.jahiaAdminUrl, flags.jahiaAdminUsername, flags.jahiaAdminPassword, moduleFilepath);
 			if (installedModule === false || installedModule.length !== 1) {
 				console.log('Error: Unable to install module');
 				exit();
@@ -42,12 +42,12 @@ const installModule = async (
 				for (const mod of otherModules ) {
 					console.log('Removal of duplicate module: ' + mod.key);
 					// eslint-disable-next-line no-await-in-loop
-					await uninstallMod(flags.jahiaAdminUrl, flags.jahiaAdminUsername, flags.jahiaAdminUsername, mod.key);
+					await uninstallMod(flags.jahiaAdminUrl, flags.jahiaAdminUsername, flags.jahiaAdminPassword, mod.key);
 				}
 				const postInstallModules = await getModules(
 					flags.jahiaAdminUrl,
 					flags.jahiaAdminUsername,
-					flags.jahiaAdminUsername
+					flags.jahiaAdminPassword
 				);
 				console.log('The following modules are on the platform for id: ' + moduleId);			
 				console.log(postInstallModules.filter((m: any) => m.id === installedModule[0].symbolicName))
